deb7c24b4b799f3d739a1737b4f3be4490e4cd0b
Lukas F <lukas.forst48@googlemail.com>
2018-09-03 13:18:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 # out the current state.
 
 def display_board(board)
-  puts  " #{board[0]} | #{board[1]} | #{board[2]} "
-puts  "-----------"
-puts  " #{board[4]} | #{board[5]} | #{board[6]} "
-puts  "-----------"
-puts  " #{board[7]} | #{board[8]} | #{board[9]} "
+  print  " #{board[0]} | #{board[1]} | #{board[2]} \n"
+  print  "-----------\n"
+  print  " #{board[4]} | #{board[5]} | #{board[6]} \n"
+  print  "-----------\n"
+  print  " #{board[7]} | #{board[8]} | #{board[9]} \n"
 end
 üöay_board = [" "," "," "," "," "," "," "," "," "]
 display_board(üöay_board)
