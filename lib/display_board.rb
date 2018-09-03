37844646837d1a9ecc022e788e97e2aad33325cd
Lukas F <lukas.forst48@googlemail.com>
2018-09-03 13:23:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@
 def display_board(board=[" "," "," "," "," "," "," "," "," "])
   print  " #{board[0]} | #{board[1]} | #{board[2]} \n"
   print  "-----------\n"
-  print  " #{board[4]} | #{board[5]} | #{board[6]} \n"
+  print  " #{board[3]} | #{board[4]} | #{board[5]} \n"
   print  "-----------\n"
-  print  " #{board[7]} | #{board[8]} | #{board[9]} \n"
+  print  " #{board[6]} | #{board[7]} | #{board[8]} \n"
 end
 üöay_board = [" "," ","X"," "," "," "," "," "," "]
 display_board(üöay_board)
