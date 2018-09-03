c7c578d68b3169f99ffeb04598cd6a010f452aa3
Lukas F <lukas.forst48@googlemail.com>
2018-09-03 13:17:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(play_board)
-print  " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board(board)
+  print  " #{board[0]} | #{board[1]} | #{board[2]} "
 print  "-----------"
 print  " #{board[4]} | #{board[5]} | #{board[6]} "
 print  "-----------"
 print  " #{board[7]} | #{board[8]} | #{board[9]} "
 end
-board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
+üöay_board = [" "," "," "," "," "," "," "," "," "]
+display_board(üöay_board)
